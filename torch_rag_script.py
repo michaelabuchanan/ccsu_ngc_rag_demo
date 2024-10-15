7c067f1dc31a2f66c29e29d4ebbe127528c4a126
Michaela Buchanan <michaela.buchanan@markiiisys.com>
2024-10-15 09:23:08 -0700

    updated script

--- torch_rag_script.py
+++ torch_rag_script.py
@@ -14,7 +14,9 @@ else:
 
 llama_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+
 tokenizer.pad_token = tokenizer.eos_token
+llama_model.generation_config.pad_token_id = tokenizer.pad_token_id
 
 if RAG:
     df = pd.read_csv("hf://datasets/Shengtao/recipe/recipe.csv")
@@ -35,8 +37,8 @@ if RAG:
 
     print("Made recipe embeddings")
 
-    prompt_task = "Question: "
-    prompt_end = "\nAnswer: "
+prompt_task = "Question: "
+prompt_end = "\nAnswer: "
 
 prompts = [
     "What two ingredients do I need for two-ingredient pizza dough?",
@@ -45,6 +47,7 @@ prompts = [
 ]
 
 for prompt in prompts:
+    prompt = prompt_task + prompt + prompt_end
     if RAG:
         prompt_embed = st_model.encode(prompt, convert_to_tensor=True)
         hits = semantic_search(prompt_embed, embeddings, top_k=2)
@@ -52,14 +55,14 @@ for prompt in prompts:
         result_line1 = hits[0][0]['corpus_id']
         result_line2 = hits[0][1]['corpus_id']
 
-        prompt = text[result_line1] + "\n" + text[result_line2] + "\n\n" +  prompt_task + prompt + prompt_end
+        prompt = text[result_line1] + "\n" + text[result_line2] + "\n\n" + prompt
     
     inputs = tokenizer(prompt, return_tensors="pt")
 
-    generate_ids = llama_model.generate(input_ids=inputs.input_ids, attention_mask=inputs.attention_mask, max_new_tokens=20)
+    generate_ids = llama_model.generate(input_ids=inputs.input_ids, attention_mask=inputs.attention_mask, max_new_tokens=25)
 
     print("\n")
     print(tokenizer.decode(generate_ids[0]))
-    print("\n------------------------------------------------------------\n")
+    print("\n------------------------------------------------------------")
 
 print("\n\nAll done")
